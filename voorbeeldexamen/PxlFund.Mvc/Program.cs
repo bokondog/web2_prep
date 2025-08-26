f2d57976bb96a992c58cf7220ccef5cf195a8c7f
bokondog <glenn.bokondo@student.pxl.be>
2025-08-26 12:53:14 +0200

    restructure appdbcontext and set up login screens

--- voorbeeldexamen/PxlFund.Mvc/Program.cs
+++ voorbeeldexamen/PxlFund.Mvc/Program.cs
@@ -1,16 +1,24 @@
+using PxlFund.Mvc.Data;
+using PxlFund.Shared.Services;
+using PxlFund.Shared.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 //Dbcontext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext")));
 
 //Identity
 
 //External Login - Google
 
 //PxlFund.Shared
-//builder.Services.AddScoped<ISeedDataRepository, SeedDataRepository>();
-//builder.Services.AddScoped<IUserLoginRepository, UserLoginRepository>();
+builder.Services.AddScoped<ISeedDataRepository, SeedDataRepository>();
+builder.Services.AddScoped<IUserLoginRepository, UserLoginRepository>();
 
 var app = builder.Build();
 
